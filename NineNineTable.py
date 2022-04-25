24ede1bbd6f8fd8274f966536808cf8ed204d0f6
Icarusing <44233384+Icarusing@users.noreply.github.com>
2022-04-25 20:02:47 +0800

    print nine nine table

---
+++ NineNineTable.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author:yxc  time:2022/4/25
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print("{}x{}={}\t".format(i,j,i*j),end="")
+    print()
